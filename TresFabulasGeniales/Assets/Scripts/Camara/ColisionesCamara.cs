ef761390c4f784f447445298c1b2f954dc3558a7
AXBNBS <44948868+AXBNBS@users.noreply.github.com>
2020-02-12 15:09:29 +0100

    Jajaja

--- TresFabulasGeniales/Assets/Scripts/Camara/ColisionesCamara.cs
+++ TresFabulasGeniales/Assets/Scripts/Camara/ColisionesCamara.cs
@@ -8,9 +8,9 @@ using UnityEngine;
 public class ColisionesCamara : MonoBehaviour
 {
     [SerializeField] private int minimoDst, maximoDst, suavizado;
-    [SerializeField] private LayerMask capas;
+    [SerializeField] private LayerMask capas, capasSinAvt;
     private float distancia;
-    private Vector3 direccion;
+    private Vector3 direccion, posicionUltFrm;
     private RaycastHit raycastDat;
 
 
@@ -19,20 +19,43 @@ public class ColisionesCamara : MonoBehaviour
     {
         direccion = this.transform.localPosition.normalized;
         distancia = this.transform.localPosition.magnitude;
+        posicionUltFrm = this.transform.position;
+        //movidoUltFrm = false;
     }
 
 
     // Si existe algún obstáculo entre la posición de la cámara y el avatar seguido, hacemos que esta se acerque al mismo.
     private void Update ()
     {
-        if (Physics.Raycast (this.transform.parent.position, this.transform.position - this.transform.parent.position, out raycastDat, maximoDst, capas, QueryTriggerInteraction.Ignore) == true)
+        if (this.transform.position != posicionUltFrm) 
         {
-            distancia = Mathf.Clamp (raycastDat.distance * 0.7f, minimoDst, maximoDst);
+            if (Physics.Raycast (this.transform.parent.position, this.transform.position - this.transform.parent.position, out raycastDat, maximoDst, CambioDePersonajesYAgrupacion.instancia.juntos == false ? capas : capasSinAvt,
+                QueryTriggerInteraction.Ignore) == true)
+            {
+                distancia = Mathf.Clamp (raycastDat.distance * 0.7f, minimoDst, maximoDst);
+            }
+            else
+            {
+                distancia = maximoDst;
+            }
+
+            /*Vector3 posicionObj = distancia * (direccion - extra);
+
+            if (Vector3.Distance (posicionObj, this.transform.parent.position) < minimoDst) 
+            {
+                posicionObj = minimoDst * direccion;
+            }*/
+            this.transform.localPosition = Vector3.Lerp (this.transform.localPosition, distancia * direccion, Time.deltaTime * suavizado);
         }
-        else 
-        {
-            distancia = maximoDst;
-        }
-        this.transform.localPosition = Vector3.Lerp (this.transform.localPosition, direccion * distancia, Time.deltaTime * suavizado);
+        posicionUltFrm = this.transform.position;
     }
+
+
+    // Pal debug.
+    /*private void OnDrawGizmos ()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere (this.transform.position, 0.5f);
+    }*/
 }
